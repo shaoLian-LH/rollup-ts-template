53e17de9e629b2f9df6bd0fa24776f8a619300ee
shaolianfk <871089057@qq.com>
2022-12-01 16:13:49 +0800

    fix: 修复 rollup 新版本打包问题

---
+++ rollup.config.mjs
@@ -0,0 +1,53 @@
+import cleaner from 'rollup-plugin-cleaner'
+import copy from 'rollup-plugin-copy'
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+import dayTool from 'dayjs'
+import fs from 'fs'
+
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf-8' }))
+
+const author = pkg.author
+const banner = `/**
+ * @license ${pkg.license}
+ * author: ${author}
+ * date: ${dayTool().format('YYYY-MM-DD')}
+ */
+`
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			exports: 'named',
+			sourcemap: false,
+			banner
+		},
+		{
+			file: pkg.module,
+			format: 'es',
+			exports: 'named',
+			sourcemap: false,
+			banner
+		}
+	],
+	plugins: [
+		copy({
+			targets: [{ src: 'src/types', dest: 'dist' }]
+		}),
+		terser(),
+		typescript({
+			tsconfigOverride: {
+				exclude: ['__test__/*', '__test__/**', 'vitest.*']
+			},
+			rollupCommonJSResolveHack: false,
+			clean: true
+		}),
+		cleaner({
+			targets: ['./dist/']
+		})
+	]
+}
